111f49eaed8eb15515d1fd565c4a522e4e72d0a1
Victor Onyekwere <cydollarsign1@gmail.com>
2024-09-20 04:58:37 +0200

    For Checkbox question to not select the same option twice and avoid nullifying first selection, add all currentSelectedOption to selectedOption array and check that currentSelectedOption is not included in selectedOptions array before selecting a new option for the checkbox

--- auto-fill-form.js
+++ auto-fill-form.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 
-let noOfFills = 10;
+let noOfFills = 2;
+let noOfOptionsToSelect
 
 //Options to cjoose
 const possibleOptions = [
@@ -11,15 +12,19 @@ const possibleOptions = [
     },
     {
         optionIds: ["id4", "id5", "id6"],
-        cb: true,
+        cb: false,
         submit: true
     }
 ];
 
 //Random selection
 function randomSelect(option) {
-    let randomOptionSelect = Math.floor(Math.random * option.length);
+    let randomOptionSelect = Math.floor(Math.random() * option.length);
     return option[randomOptionSelect];
+}
+
+function delay (fillInterval) {
+    setTimeout(randomSelect , fillInterval);
 }
 
 //Automate Fill
@@ -29,11 +34,17 @@ async function fillForm(page) {
 
         //If it is a Checkbox Question
         if (cb) {
-            let optionsToSelect = []
-            let noOfOptionsToSelect = Math.floor(Math.random() * 5) || 2 //optionIds.length
-            console.log(noOfOptionsToSelect)
-            for (let i = 0; i < noOfOptionsToSelect; i++) {
-
+            let selectedOptions = []
+            noOfOptionsToSelect = Math.round(Math.random() * optionIds.length) || 2 //optionIds.length
+            
+            console.log("First NoOfC", noOfOptionsToSelect)
+            for (let i = 0; i < noOfOptionsToSelect; i = i+1) {
+                let currentSelectedOption
+                while (selectedOptions.includes(currentSelectedOption)) {
+                currentSelectedOption = randomSelect(optionIds)
+                    console.log(currentSelectedOption, selectedOptions)
+                }
+                selectedOptions.push(currentSelectedOption);
             }
         }
     }
