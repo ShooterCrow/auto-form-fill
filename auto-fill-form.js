c05a6146870da65e25b672aebdcb6fd2bb3b176c
Victor Onyekwere <cydollarsign1@gmail.com>
2024-09-25 07:48:31 +0200

    OPtion structure and random select

--- auto-fill-form.js
+++ auto-fill-form.js
@@ -1,9 +1,26 @@
 const puppeteer = require("puppeteer")
 
 //Options to cjoose
-const possibleOptions = [];
+const possibleOptions = [
+    {
+        optionIds: ["id1", "id2", "id3"],
+        cb: false,
+        submit: false
+    },
+    {
+        optionIds: ["id4", "id5", "id6"],
+        cb: false,
+        submit: true
+    }
+];
 
 //Random selection
-function randomSelect () {
-    
+function randomSelect(option) {
+    let randomOptionSelect = Math.floor(Math.random * option.length)
+    return option[randomOptionSelect];
+}
+
+//Automate Fill
+async function fillForm(page) {
+
 }
