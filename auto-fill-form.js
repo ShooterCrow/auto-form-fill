0a5dbe9f14e19ca3f4f8d3be11f37ed25dcd9fc8
Victor Onyekwere <cydollarsign1@gmail.com>
2024-09-25 08:40:15 +0200

    If it is a checkbox question select multiple by first determining the number of options to select and store in noOfOptionsToSelect

--- auto-fill-form.js
+++ auto-fill-form.js
@@ -1,26 +1,47 @@
-const puppeteer = require("puppeteer")
+const puppeteer = require("puppeteer");
+
+let noOfFills = 10;
 
 //Options to cjoose
 const possibleOptions = [
     {
         optionIds: ["id1", "id2", "id3"],
-        cb: false,
+        cb: true,
         submit: false
     },
     {
         optionIds: ["id4", "id5", "id6"],
-        cb: false,
+        cb: true,
         submit: true
     }
 ];
 
 //Random selection
 function randomSelect(option) {
-    let randomOptionSelect = Math.floor(Math.random * option.length)
+    let randomOptionSelect = Math.floor(Math.random * option.length);
     return option[randomOptionSelect];
 }
 
 //Automate Fill
 async function fillForm(page) {
+    for (const options of possibleOptions) {
+        const { optionIds, cb, submit } = options
 
+        //If it is a Checkbox Question
+        if (cb) {
+            let optionsToSelect = []
+            let noOfOptionsToSelect = Math.floor(Math.random() * 5) || 2 //optionIds.length
+            console.log(noOfOptionsToSelect)
+            for (let i = 0; i < noOfOptionsToSelect; i++) {
+
+            }
+        }
+    }
 }
+
+async function multipleFill () {
+    for (let i = 0; i < noOfFills; i++) {
+        fillForm()
+    }
+}
+multipleFill()
